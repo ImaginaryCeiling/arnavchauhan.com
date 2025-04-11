b5e0ae9c0031909de6a592afbd25a2635e7e4b0c
Arnav Chauhan <arnav216@gmail.com>
2025-04-11 11:03:07 -0400

    add new project, add youtube and github link space on project card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,8 @@
 import Image from './Image'
 import Link from './Link'
+import SocialIcon from './social-icons'
 
-const Card = ({ title, description, imgSrc, href }) => (
+const Card = ({ title, description, imgSrc, href, github, youtube }) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
     <div
       className={`${
@@ -31,7 +32,7 @@ const Card = ({ title, description, imgSrc, href }) => (
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
+            <Link href={href} aria-label={`Link to ${title}` } className="underline">
               {title}
             </Link>
           ) : (
@@ -39,15 +40,14 @@ const Card = ({ title, description, imgSrc, href }) => (
           )}
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
-        )}
+        <div className="flex space-x-3">
+          {/* Add SocialIcon components here */}
+          {/* Example: <SocialIcon kind="github" href="https://github.com/username" /> */}
+          {github && (<SocialIcon kind='github' href={github} />)}
+          {youtube && (<SocialIcon kind='youtube' href={youtube} />)}
+
+        </div>
+        
       </div>
     </div>
   </div>
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,16 +1,22 @@
 interface Project {
   title: string,
   description: string,
-  href?: string,
   imgSrc?: string,
+  href: string,
+  github?: string,
+  youtube?: string,
+
 }
 
 const projectsData: Project[] = [
   {
     title: "Poker? I hardly know her!",
     description: "A Meta Ray-Ban Glasses-powered system that lets you play poker and do homework at the same time—but the better you do at one, the worse you perform at the other.",
+    imgSrc: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/6d8ec4172c2d78efe3d8e30a3b9f8f23a7beb001_scr-20250404-dctf.jpeg",
     href: "https://github.com/sahitid/meta-vision-project",
-    imgSrc: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/6d8ec4172c2d78efe3d8e30a3b9f8f23a7beb001_scr-20250404-dctf.jpeg"
+    github: "https://github.com/sahitid/meta-vision-project",
+    youtube: "https://www.youtube.com/watch?v=JVtFxCJw5ng",
+
 
   }
   
