276bbb481210a32fae2a7426330b7202bef08328
Arnav Chauhan <arnav216@gmail.com>
2025-10-08 19:38:18 -0400

    a few more books

--- data/booksData.ts
+++ data/booksData.ts
@@ -8,6 +8,13 @@ const booksData: (string | BookEntry)[] = [
   'Animal Farm',
   'Atomic Habits',
   'The Old Man and the Sea',
+  '1984',
+  'The Great Gatsby',
+  'Ikigai',
+  "Can't Hurt Me",
+  'Shoe Dog',
+  'Fahrenheit 451',
+
   // Add more book titles here
   // Or use custom format:
   // {
