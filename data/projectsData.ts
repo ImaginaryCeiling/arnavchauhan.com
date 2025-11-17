d30e8e5e18b72f282f3514070fbb9047b54f24d6
Arnav Chauhan <arnav216@gmail.com>
2025-11-17 12:41:23 -0500

    update project order

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,23 +9,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Poker? I hardly know her!',
+    title: 'Vitalis Health',
     description:
-      'A Meta Ray-Ban Glasses-powered system that lets you play poker and do homework at the same time—but the better you do at one, the worse you perform at the other.',
-    imgSrc:
-      'https://hc-cdn.hel1.your-objectstorage.com/s/v3/6d8ec4172c2d78efe3d8e30a3b9f8f23a7beb001_scr-20250404-dctf.jpeg',
-    href: 'https://github.com/sahitid/meta-vision-project',
-    github: 'https://github.com/sahitid/meta-vision-project',
-    youtube: 'https://www.youtube.com/watch?v=JVtFxCJw5ng',
-  },
-  {
-    title: 'DECA 2025 - SEOR - Cary Tennis Park',
-    description:
-      'Our DECA paper for the Sports and Entertainment Operations Research Event from 2025, where we analyzed operations at Cary Tennis Park and proposed a plan to implement AI at the facility.',
-    imgSrc:
-      'https://www.timmons.com/wp-content/uploads/2020/01/web_cary-tennis-park-110222-image-seq-019.jpg',
-    href: 'https://deca2025.vercel.app',
-    github: 'https://github.com/ImaginaryCeiling/deca_site',
+      '1st Place in the Healthcare track at HackNYU 2025. Built a central OS for nurses to monitor their hospital floors easier and spend more time taking care of their patients. Snuck into an emergency room and found a nurse to interview to help us make the product.',
+    imgSrc: '/static/images/vitalis.png',
+    href: 'https://main.d204vv20o17ygk.amplifyapp.com/',
+    github: 'https://github.com/SupratikPanuganti/HackNYU',
   },
   {
     title: 'MBDump💩',
@@ -36,20 +25,22 @@ const projectsData: Project[] = [
     github: 'https://github.com/ImaginaryCeiling/MBDump',
   },
   {
+    title: 'Poker? I hardly know her!',
+    description:
+      'A Meta Ray-Ban Glasses-powered system that lets you play poker and do homework at the same time—but the better you do at one, the worse you perform at the other.',
+    imgSrc:
+      'https://hc-cdn.hel1.your-objectstorage.com/s/v3/6d8ec4172c2d78efe3d8e30a3b9f8f23a7beb001_scr-20250404-dctf.jpeg',
+    href: 'https://github.com/sahitid/meta-vision-project',
+    github: 'https://github.com/sahitid/meta-vision-project',
+    youtube: 'https://www.youtube.com/watch?v=JVtFxCJw5ng',
+  },
+  {
     title: 'Frog Population Simulator',
     description:
       'Got bored and started making simulations for my teachers to use in their classes. Now making tools for any teacher to use natural language to generate their own custom simulations instantly.',
     imgSrc: '/static/images/frog-population-simulator.png',
     href: 'https://frog-population.streamlit.app/',
     github: 'https://github.com/ImaginaryCeiling/frog-population-simulator',
-  },
-  {
-    title: 'Vitalis Health',
-    description:
-      '1st Place in the Healthcare track at HackNYU 2025. Built a central OS for nurses to monitor their hospital floors easier and spend more time taking care of their patients. Snuck into an emergency room and found a nurse to interview to help us make the product.',
-    imgSrc: '/static/images/vitalis.png',
-    href: 'https://main.d204vv20o17ygk.amplifyapp.com/',
-    github: 'https://github.com/SupratikPanuganti/HackNYU',
   },
 ]
 
