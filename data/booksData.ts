396ff853cb8aa5303bdd5a2d53864ba499af29e2
Arnav Chauhan <arnav216@gmail.com>
2026-01-03 12:55:35 +0530

    update book

--- data/booksData.ts
+++ data/booksData.ts
@@ -10,16 +10,18 @@ export interface BookEntry {
 
 const booksData: (string | BookEntry)[] = [
   {
-    title: 'The Psychology of Money',
-    author: 'Morgan Housel',
-    toRead: true,
-  },
-  {
     title: 'What The Dog Saw And Other Adventures',
     author: 'Malcom Gladwell',
     coverUrl: 'https://m.media-amazon.com/images/I/61WvcA51fzL._AC_UF1000,1000_QL80_.jpg',
     goodreadsUrl:
       'https://www.goodreads.com/book/show/6516450-what-the-dog-saw-and-other-adventures',
+    toRead: true,
+  },
+  {
+    title: "Lilliput Land: How Small is Driving India's Mega Consumption Story",
+    author: 'Rama Bijapurkar',
+    coverUrl: 'https://m.media-amazon.com/images/I/617JmWhOjkL._SY522_.jpg',
+    goodreadsUrl: 'https://www.goodreads.com/en/book/show/207639030-lilliput-land',
     currentlyReading: true,
   },
   {
